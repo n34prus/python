885714f838f0a9e4c81c42d860fde6fe70235f3c
n34prus <n34prus@gmail.com>
2022-02-01 15:58:31 +0400

    - вывел самые отменяемые месяцы для CityHotel - вывел наиболее заселяемую возрастную группу - вывел наиболее поплярный тип отеля для клиентов с детьми - сравнил отток клиентов для двух групп: с детьми и без - починил кодировку в 3_hotel_readme.txt

--- education/stepic-analyse/3_hotel.py
+++ education/stepic-analyse/3_hotel.py
@@ -4,6 +4,10 @@ import requests
 import pandas as pd
 from datetime import datetime
 
+#debug_mode hiding output
+#set debug_mode to False for looking to full result
+debug_mode = False
+
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 20)
 
@@ -54,7 +58,8 @@ sucsess_rate = bookings \
     .sort_values('is_canceled', ascending=False)
 sucsess_rate = sucsess_rate.rename(columns={'is_canceled': 'is_sucsess'})
 sucsess_rate = sucsess_rate.reset_index(drop=True)
-#print('\n1. Страны с наибольшим чисом успешных бронирований:\n', sucsess_rate.head(5))
+if not debug_mode:
+    print('\n1. Страны с наибольшим чисом успешных бронирований:\n', sucsess_rate.head(5))
 
 #2. На сколько ночей в среднем бронируют отели разных типов? точность 2 знака
 ch_rh_mean = bookings \
@@ -62,12 +67,14 @@ ch_rh_mean = bookings \
     .agg({'stays_total_nights': 'mean'}) \
     .round(2)
 ch_rh_mean = ch_rh_mean.rename(columns={'stays_total_nights': 'stays_mean_nights'})
-#print('\n2. Средняя длительность брони для разных типов отелей:\n', ch_rh_mean)
+if not debug_mode:
+    print('\n2. Средняя длительность брони для разных типов отелей:\n', ch_rh_mean)
 
 #3. Иногда тип номера, полученного клиентом (assigned_room_type), отличается от изначально забронированного (reserved_room_type).
 # Такое может произойти, например, по причине овербукинга. Сколько подобных наблюдений встретилось в датасете?
 overbooking = bookings.query('assigned_room_type != reserved_room_type').shape[0]
-#print('\n3. Количество случаев заселения клиента в тип номера, отличный от забронированного: {} ({}%)'.format(overbooking, round(overbooking/bookings.shape[0], 2)))
+if not debug_mode:
+    print('\n3. Количество случаев заселения клиента в тип номера, отличный от забронированного: {} ({}%)'.format(overbooking, round(overbooking/bookings.shape[0], 2)))
 
 #4. На какой месяц чаще всего успешно оформляли бронь в 2016? Изменился ли самый популярный месяц в 2017?
 #слишком частный пример. лучше сразу написать код для всех годов в датасете
@@ -95,8 +102,59 @@ if (month_rate['arrival_date_month'][fvi2016] == month_rate['arrival_date_month'
     answer421 = 'не '
 else:
     answer422 = 'на: {}'.format(month_rate['arrival_date_month'][fvi2017])
-print('\n4. Самые популярные месяцы в разные годы:\n', month_rate)
-print('\n4.1 Самый популярный месяц в 2016: ', month_rate['arrival_date_month'][fvi2016])
-print(f'4.2 Самый популярный месяц в 2017 {answer421}изменился {answer422}')
-#print(bookings)
+if not debug_mode:
+    print('\n4. Самые популярные месяцы в разные годы:\n', month_rate)
+    print('\n4.1 Самый популярный месяц в 2016: ', month_rate['arrival_date_month'][fvi2016])
+    print(f'4.2 Самый популярный месяц в 2017 {answer421}изменился {answer422}')
+
+#5. На какой месяц бронирования отеля типа City Hotel отменялись чаще всего в каждый из годов?
+#теперь группируем по отказам
+month_cancel_rate = bookings \
+    .query("is_canceled == 1 and hotel == 'City Hotel'") \
+    .groupby(['arrival_date_month', 'arrival_date_year'], as_index=False) \
+    .agg({'is_canceled': 'count'}) \
+    .sort_values('is_canceled', ascending=False)
+month_cancel_rate = month_cancel_rate \
+    .groupby(['arrival_date_year'], as_index=False) \
+    .agg({'is_canceled': 'max', 'arrival_date_month': 'first'}) \
+    .sort_values('arrival_date_year', ascending=False)
+month_cancel_rate = month_cancel_rate.reset_index(drop=True)
+if not debug_mode:
+    print('\n5. Самые "отмеяемые" месяцы для CityHotel в разные годы:\n', month_cancel_rate)
+
+#6. Посмотрите на числовые характеристики трёх переменных: adults, children и babies. Какая из них имеет наибольшее среднее значение?
+#тут делаем агрегирование, функцию запиливаем в виде листа чтобы была таблица с подписями. транспонируем, сортируем
+age_mean = bookings \
+    .agg({'adults': ['mean'], 'children': ['mean'], 'babies': ['mean']}) \
+    .T \
+    .sort_values('mean', ascending=False)
+if not debug_mode:
+    print('\n6. Средние значения для брони по возрасту клиента:\n', age_mean)
+    print('\n6.1 Чаще всего заселяются: ', age_mean.first_valid_index())
+
+#7. Создайте колонку total_kids, объединив children и babies. Отели какого типа в среднем пользуются большей популярностью у клиентов с детьми?
+bookings['total_kids'] = bookings['children'] + bookings['babies']
+kids_rate = bookings \
+    .groupby(['hotel'], as_index=False) \
+    .agg({'total_kids': 'mean'}) \
+    .sort_values('total_kids', ascending=False)
+kids_rate = kids_rate.rename(columns={'total_kids': 'mean_kids'})
+kids_rate = kids_rate.reset_index(drop=True)
+if not debug_mode:
+    print('\n7. Детей чаще заселяют в отель типа {}'.format(kids_rate['hotel'][0]))
+    print('7.1 Для этих отелей среднее количество детей на бронь составлет {} штук'.format(kids_rate['mean_kids'][0].round(2)))
+
+#8. Создайте колонку has_kids, сравните % оттока клиентов в двух группах: с детьми и без
+bookings['has_kids'] = bookings['total_kids'] > 0
+churn_rate = bookings \
+    .groupby(['has_kids'], as_index=False) \
+    .agg({'is_canceled': 'mean'}) \
+    .sort_values('is_canceled', ascending=False)
+if churn_rate['has_kids'][0] == True:
+    _result = 'с детьми'
+else:
+    _result = 'без детей'
+if not debug_mode:
+    print('\n8. Отток выше у клиентов {}.'.format(_result))
+    print('8.1 Для этих клиентов отток составлет {}%\n'.format((churn_rate['is_canceled'][0]*100).round(2)))
 
